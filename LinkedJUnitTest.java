1c32663fe91fd1eaee3d66e0bb2b5cc16136b986
Joshua Lai <jdlai@cpp.edu>
2021-10-08 13:24:26 -0700

    added JUnit test cases for convertToPostfix

--- LinkedJUnitTest.java
+++ LinkedJUnitTest.java
@@ -1,5 +1,3 @@
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 import junit.framework.TestCase;
@@ -7,19 +5,50 @@ import project2.LinkedStack;
 
 public class LinkedJUnitTest extends TestCase
 {
-   String[] infixExpressions = {"a+b"};
-   String[] expectedResults = {"ab+"};
-   @Override
-   protected void setUp() throws Exception
+   @Test
+   //Various assorted inputs and expected outputs.
+   public void testConvertToPostfix()
    {
+      String[] infixExpressions = {"a+b", "a*c+b-(e-d)", "a*b/(c-a)+d*e"};
+      String[] expectedResults = {"ab+", "ac*b+ed--", "ab*ca-/de*+"};
+      for(int i = 0; i < infixExpressions.length; i++)
+      {
+         assertEquals("test", expectedResults[i], LinkedStack.convertToPostfix(infixExpressions[i]));
+      }
+   }
+   @Test
+   //Should be able to handle if the expression is blank.
+   public void testEmpty()
+   {
+      String infixExpression = "";
+      String expectedResult = "";
+      assertEquals(expectedResult, LinkedStack.convertToPostfix(infixExpression));
    }
 
    @Test
-   public void testConvertToPostfix()
+   //Should be able to handle if the expression has spaces in it.
+   public void testWithSpaces()
    {
-      for(int i = 0; i < infixExpressions.length; i++)
-      {
-         assertEquals(expectedResults[i], LinkedStack.convertToPostfix(infixExpressions[i]));
-      }
+      String infixExpression = "a * b / (c - a) + d * e";
+      String expectedResult = "ab*ca-/de*+";
+      assertEquals(expectedResult, LinkedStack.convertToPostfix(infixExpression));
+   }
+
+   @Test
+   //Should be able to handle if the expression has numbers instead of characters.
+   public void testWithNumbers()
+   {
+      String infixExpression = "2*b/(c-2)+d*6";
+      String expectedResult = "2b*c2-/d6*+";
+      assertEquals(expectedResult, LinkedStack.convertToPostfix(infixExpression));
+   }
+
+   @Test
+   //Should be able to handle if the expression has invalid operators mixed in.
+   public void testWithInvalids()
+   {
+      String infixExpression = "a*& b /#@(c-$a)+ ?d<* e!";
+      String expectedResult = "ab*ca-/de*+";
+      assertEquals(expectedResult, LinkedStack.convertToPostfix(infixExpression));
    }
 }
--- ResizableJUnitTest.java
+++ ResizableJUnitTest.java
@@ -3,12 +3,19 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import junit.framework.TestCase;
+import project2.ResizableArrayStack;
 
 public class ResizableJUnitTest extends TestCase
 {
-
-   @Override
-   protected void setUp() throws Exception
+   @Test
+   //Various assorted inputs and expected outputs.
+   public void testEvaluatePostfix()
    {
+      String[] postfixExpressions = {""};
+      String[] expectedResults = {""};
+      for(int i = 0; i < postfixExpressions.length; i++)
+      {
+         //assertEquals(expectedResults[i], ResizableArrayStack.evaluatePostfix(postfixExpressions[i]);
+      }
    }
 }
